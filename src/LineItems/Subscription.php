ccaef43063888f545218d6ab82395b18eff39587
Thomas Carney <thomas@chartmogul.com>
2019-09-23 14:40:15 +0200

    Add support for subcription sets (#54)

--- src/LineItems/Subscription.php
+++ src/LineItems/Subscription.php
@@ -10,6 +10,7 @@ class Subscription extends AbstractLineItem
 
     public $type = 'subscription';
     public $subscription_external_id;
+    public $subscription_set_external_id;
     public $service_period_start;
     public $service_period_end;
     public $cancelled_at;
