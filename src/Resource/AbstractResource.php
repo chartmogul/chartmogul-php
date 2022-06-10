0c7997a1936835e7a29410c1d5498aa637b77fdf
Dina Vojinovic <dina@chartmogul.com>
2022-06-10 15:38:30 +0200

    meta key handled, params in destroy and update modified.

--- src/Resource/AbstractResource.php
+++ src/Resource/AbstractResource.php
@@ -77,49 +77,63 @@ abstract class AbstractResource extends AbstractModel
                     return static::fromArray($data, $client);
                 }, $data[static::ROOT_KEY]));
                 // The following are subject to change soon, so they are optional.
-                if (isset($data["current_page"])) {
-                    $array->current_page = $data["current_page"];
-                }
-                if (isset($data["total_pages"])) {
-                    $array->total_pages = $data["total_pages"];
-                }
-                if (isset($data["has_more"])) {
-                    $array->has_more = $data["has_more"];
-                }
-                if (isset($data["per_page"])) {
-                    $array->per_page = $data["per_page"];
-                }
-                if (isset($data["page"])) {
-                    $array->page = $data["page"];
-                }
+                $array = static::allData($data, $array);
             } else {
                 $array = new SubscriptionEventCollection(array_map(function ($data) use ($client) {
                     return static::fromArray($data, $client);
                 }, $data[static::ROOT_KEY]));
+
                 if (isset($data["meta"])) {
                     $meta = $data['meta'];
-                    $array->meta = new MetaCollection();
-
-                    if (isset($meta['page'])) {
-                        $array->meta->page = $meta["page"];
-                    }
-                    if (isset($meta['next_key'])) {
-                        $array->meta->next_key = $meta["next_key"];
-                    }
-                    if (isset($meta['prev_key'])) {
-                        $array->meta->prev_key = $meta["prev_key"];
-                    }
-                    if (isset($meta['before_key'])) {
-                        $array->meta->before_key = $meta["before_key"];
-                    }
-                    if (isset($meta['total_pages'])) {
-                        $array->meta->total_pages = $meta["total_pages"];
-                    }
+                    $array->meta = static::metaData($meta);
                 }
             }
             return $array;
         }
 
         return new static($data, $client);
+    }
+
+    public static function allData(array $data, Collection $array)
+    {
+        if (isset($data["current_page"])) {
+            $array->current_page = $data["current_page"];
+        }
+        if (isset($data["total_pages"])) {
+            $array->total_pages = $data["total_pages"];
+        }
+        if (isset($data["has_more"])) {
+            $array->has_more = $data["has_more"];
+        }
+        if (isset($data["per_page"])) {
+            $array->per_page = $data["per_page"];
+        }
+        if (isset($data["page"])) {
+            $array->page = $data["page"];
+        }
+
+        return $array;
+    }
+
+    public static function metaData(array $meta)
+    {
+        $array_meta = new MetaCollection();
+        if (isset($meta['page'])) {
+            $array_meta->page = $meta["page"];
+        }
+        if (isset($meta['next_key'])) {
+            $array_meta->next_key = $meta["next_key"];
+        }
+        if (isset($meta['prev_key'])) {
+            $array_meta->prev_key = $meta["prev_key"];
+        }
+        if (isset($meta['before_key'])) {
+            $array_meta->before_key = $meta["before_key"];
+        }
+        if (isset($meta['total_pages'])) {
+            $array_meta->total_pages = $meta["total_pages"];
+        }
+
+        return $array_meta;
     }
 }
