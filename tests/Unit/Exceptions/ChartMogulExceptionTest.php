b2c026b1fd9c4d45cc4babd25feb37615a4e0960
sergio <sgallego@dfutura.com>
2023-09-18 17:07:10 +0200

    add php8.2, Fix test and install nyholm/psr7

--- tests/Unit/Exceptions/ChartMogulExceptionTest.php
+++ tests/Unit/Exceptions/ChartMogulExceptionTest.php
@@ -2,36 +2,28 @@
 namespace ChartMogul\Tests\Exceptions;
 
 use ChartMogul\Exceptions\ChartMogulException;
-use Http\Discovery\MessageFactoryDiscovery;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7\Response;
 
 class ChartMogulExceptionTest extends \PHPUnit\Framework\TestCase
 {
 
-    public function testConstruct()
-    {
+	public function testConstruct()
+	{
+		$psr17Factory = new Psr17Factory();
 
-        // Test non-JSON response
-        $mock = MessageFactoryDiscovery::find()->createResponse(
-            200,
-            null,
-            [],
-            'plain text'
-        );
-        $e = new ChartMogulException('message', $mock);
-        $this->assertEquals($e->getStatusCode(), 200);
-        $this->assertEquals($e->getResponse(), 'plain text');
+		// Test non-JSON response
+		$mock = new Response(200, [], 'plain text');
+		$e = new ChartMogulException('message', $mock);
+		$this->assertEquals($e->getStatusCode(), 200);
+		$this->assertEquals($e->getResponse(), 'plain text');
 
-        // Test JSON response
-        $mock = MessageFactoryDiscovery::find()->createResponse(
-            200,
-            null,
-            [],
-            '{"result": "json"}'
-        );
-        $e = new ChartMogulException('message', $mock);
-        $this->assertEquals($e->getStatusCode(), 200);
-        $this->assertEquals($e->getResponse(), [
-            "result" => "json"
-        ]);
-    }
+		// Test JSON response
+		$mock = new Response(200, [], '{"result": "json"}');
+		$e = new ChartMogulException('message', $mock);
+		$this->assertEquals($e->getStatusCode(), 200);
+		$this->assertEquals($e->getResponse(), [
+			"result" => "json"
+		]);
+	}
 }
