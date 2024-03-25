82b1e26416873db49fa6f9b76802ea005c83ddc8
Kamil Pavlicko <kamil.pavlicko@gmail.com>
2024-03-25 23:17:50 +0700

    Release v6.3.0

--- tests/Unit/Http/ClientTest.php
+++ tests/Unit/Http/ClientTest.php
@@ -35,7 +35,7 @@ class ClientTest extends TestCase
             ->onlyMethods([])
             ->getMock();
 
-        $this->assertEquals("chartmogul-php/6.2.0/PHP-".PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION, $mock->getUserAgent());
+        $this->assertEquals("chartmogul-php/6.3.0/PHP-".PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION, $mock->getUserAgent());
     }
 
     public function testGetBasicAuthHeader()
